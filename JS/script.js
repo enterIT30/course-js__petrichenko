bb92a1a4d82d5822b3a555c27a339b3239149b00
Vladyslav <ryzhykovvv@gmail.com>
2022-04-23 22:11:26 +0300

    completed the first practical task

---
+++ JS/script.js
@@ -0,0 +1,21 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', 4),
+      lastFilm = prompt('Один из последних просмотренных фильмов?', 'godfather'),
+      scoreFilm = +prompt('На сколько оцените его?', 8),
+      lastFilmTwo = prompt('Один из последних просмотренных фильмов?', 'godfather'),
+      scoreFilmTwo = +prompt('На сколько оцените его?', 8);
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+personalMovieDB.movies[lastFilm] = scoreFilm;
+personalMovieDB.movies[lastFilmTwo] = scoreFilmTwo;
+
+console.log(personalMovieDB);
+console.log(personalMovieDB.movies);
+
+
