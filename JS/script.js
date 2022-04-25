0416c7ca9ad4484a1f64f0f066577952ee42b2b8
Vladyslav <ryzhykovvv@gmail.com>
2022-04-25 18:32:41 +0300

    completed the second practical task

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,4 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', 4),
-      lastFilm = prompt('Один из последних просмотренных фильмов?', 'godfather'),
-      scoreFilm = +prompt('На сколько оцените его?', 8),
-      lastFilmTwo = prompt('Один из последних просмотренных фильмов?', 'godfather'),
-      scoreFilmTwo = +prompt('На сколько оцените его?', 8);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', 4);
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -12,8 +8,32 @@ const personalMovieDB = {
   privat: false,
 };
 
-personalMovieDB.movies[lastFilm] = scoreFilm;
-personalMovieDB.movies[lastFilmTwo] = scoreFilmTwo;
+
+for (let i = 0; i < 2; i++) {
+  let lastFilm = prompt('Один из последних просмотренных фильмов?', 'godfather');
+  let scoreFilm = prompt('На сколько оцените его?', 8);
+
+  if (lastFilm != null && scoreFilm != null &&
+      lastFilm != '' && scoreFilm != '' &&
+      lastFilm.length < 50  && scoreFilm <= 10 && scoreFilm >= 0) {
+        personalMovieDB.movies[lastFilm] = scoreFilm;
+        console.log('done');
+  } else {
+    console.log('error');
+    i--;
+  }
+}
+
+if (personalMovieDB.count < 10) {
+  alert('Просмотренно довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+  alert('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+  alert('Вы киноман');
+} else {
+  alert('Произошла ошибка');
+}
+
 
 console.log(personalMovieDB);
 console.log(personalMovieDB.movies);
