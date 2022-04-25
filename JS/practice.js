4222879b184489462d1cdd8edc7646c31bf4eff8
Vladyslav <ryzhykovvv@gmail.com>
2022-04-25 13:57:41 +0300

    solved a problem with a geometric figure

--- JS/practice.js
+++ JS/practice.js
@@ -1,4 +1,4 @@
-//todo ЗАДАНИЯ НА ИСПОЛЬЗОВАНИЕ ЦИКЛОВ И УСЛОВИЙ=====================================
+//todo ЗАДАНИЯ НА ИСПОЛЬЗОВАНИЕ ЦИКЛОВ И УСЛОВИЙ=======================================
 
 // 1 ==================================================================================
 
@@ -41,7 +41,6 @@ function thirdTask() {
 // }
 
 function fourthTask() {
-  // Пишем решение вот тут
   let i = 2;
 
   while ( i <= 16) {
@@ -60,7 +59,6 @@ function fourthTask() {
 function fifthTask() {
   const arrayOfNumbers = [];
 
-  // Пишем решение вот тут
   let index = 0;
 
   for (let i = 5; i <= 10; i++) {
@@ -68,27 +66,23 @@ function fifthTask() {
     index +=1;
   }
 
-  // Не трогаем
   return arrayOfNumbers;
 }
 
-//todo ПРОДВИНУТЫЕ ЗАДАНИЯ НА ЗНАНИЯ ЦИКЛОВ И УСЛОВИЙ================================
+//todo ПРОДВИНУТЫЕ ЗАДАНИЯ НА ЗНАНИЯ ЦИКЛОВ И УСЛОВИЙ==================================
 
 
 // 1 ==================================================================================
 
 function firstTask() {
-  // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
   const arr = [3, 5, 8, 16, 20, 23, 50];
   const result = [];
 
-  // Пишем решение вот тут
   for (let i = 0; i < arr.length; i++) {
     let el = arr[i];
     result[i] = el;
   }
 
-  // Не трогаем
   return result;
 }
 
@@ -96,12 +90,9 @@ console.log(firstTask());
 
 // 2 ==================================================================================
 
-// Место для второй задачи
 function secondTask() {
-  // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
   const data = [5, 10, 'Shopping', 20, 'Homework'];
 
-  // Пишем решение вот тут
   for (let i = 0; i < data.length; i++) {
     if (typeof(data[i]) === 'number') {
       data[i] *= 2;
@@ -109,7 +100,7 @@ function secondTask() {
       data[i] = `${data[i]}-done`;
     }
   }
-  // Не трогаем
+
   return data;
 }
 
@@ -119,16 +110,13 @@ console.log(secondTask());
 // 3 ==================================================================================
 
 function thirdTask() {
-  // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
   const data = [5, 10, 'Shopping', 20, 'Homework'];
   const result = [];
 
-  // Пишем решение вот тут
   for (let i = 0; i <= data.length; i++) {
     result[i] = data[data.length - i]
 }
 
-  // Не трогаем
   return result;
 }
 console.log(thirdTask());
@@ -138,3 +126,221 @@ console.log(thirdTask());
         result[i - 1] = data[data.length - i]
     }
 */
+
+//todo ЗАДАЧА НА ФОРМИРОВАНИЕ ФИГУРЫ ===================================================
+
+// ПОЛОВИНКА ЁЛОЧКИ=====================================================================
+
+/*
+
+*
+**
+***
+****
+*****
+******
+
+*/
+
+const lines = 6;
+let result = '';
+
+for (let i = 1; i <= lines; i++) {
+  for (let j = 0; j < i; j++) {
+    result += '*';
+  }
+
+  result += '\n';
+}
+
+console.log(result);
+
+
+// ОБРАТНАЯ ПОЛОВИНКА ЁЛОЧКИ ============================================================
+
+/*
+
+     *
+    **
+   ***
+  ****
+ *****
+******
+
+ */
+
+const lines = 6;
+let result = '';
+
+for (let i = 1; i <= lines; i++) {
+  for (let j = i; j < lines; j++) {
+    result += ' ';
+  }
+  for (let k = 0; k < i; k++) {
+
+    result += '*';
+  }
+
+  result += '\n';
+}
+
+console.log(result);
+
+
+
+// МОЙ 1-Й ВАРИАНТ ЦЕЛОЙ ЁЛОЧКИ (3 ВЛОЖЕННЫХ ЦИКЛА В ОСНОВНОМ ЦИКЛЕ) ===================
+
+/*
+
+     *
+    ***
+   *****
+  *******
+ *********
+***********
+
+ */
+
+const lines = 6;
+let result = '';
+
+for (let i = 1; i <= lines; i++) {
+  for (let j = i; j < lines; j++) {
+    result += ' ';
+  }
+  for (let k = 0; k < i; k++) {
+    result += '*';
+  }
+  for (let r = 1; r < i; r++) {
+    result += '*';
+  }
+
+
+  result += '\n';
+}
+
+console.log(result);
+
+
+
+// МОЙ 2-Й ВАРИАНТ ЦЕЛОЙ ЁЛОЧКИ (2 ВЛОЖЕННЫХ ЦИКЛА В ОСНОВНОМ ЦИКЛЕ) ===================
+
+const lines = 6;
+let result = '';
+
+for (let i = 1; i <= lines; i++) {
+  for (let j = i; j < lines; j++) {
+    result += ' ';
+  }
+  for (let k = 0; k <= ((i * 2) - 2); k++) {
+
+    result += '*';
+  }
+
+
+  result += '\n';
+}
+
+console.log(result);
+
+
+//! ОТВЕТ ЛЕКТОРА (2 ВЛОЖЕННЫХ ЦИКЛА В ОСНОВНОМ ЦИКЛЕ) =================================
+
+const lines = 5;
+let result = '';
+
+for (let i = 0; i <= lines; i++) {
+    for (let j = 0; j < lines - i; j++) {
+        result += " ";
+    }
+    for (let k = 0; k < 2 * i + 1; k++) {
+        result += "*";
+    }
+    result += "\n";
+}
+
+
+console.log(result);
+
+
+// МЕТКИ ===============================================================================
+
+for (let i = 0; i < 3; i++) {
+  console.log(`First cycle: ${i}`);
+  for (let j = 0; j < 3; j++) {
+    console.log(`Second cycle: ${j}`);
+    for (let k = 0; k < 3; k++) {
+      console.log(`Third cycle: ${k}`);
+    }
+  }
+}
+
+first: for (let i = 0; i < 3; i++) {
+  console.log(`First cycle: ${i}`);
+  for (let j = 0; j < 3; j++) {
+    console.log(`Second cycle: ${j}`);
+    for (let k = 0; k < 3; k++) {
+      if (k === 2) {
+        continue first;
+      }
+      console.log(`Third cycle: ${k}`);
+    }
+  }
+}
+
+for (let i = 0; i < 3; i++) {
+  console.log(`First cycle: ${i}`);
+  second: for (let j = 0; j < 3; j++) {
+    console.log(`Second cycle: ${j}`);
+    for (let k = 0; k < 3; k++) {
+      if (k === 2) {
+        continue second;
+      }
+      console.log(`Third cycle: ${k}`);
+    }
+  }
+}
+
+/* 
+First cycle: 0      First cycle: 0      First cycle: 0
+Second cycle: 0     Second cycle: 0     Second cycle: 0
+Third cycle: 0      Third cycle: 0      Third cycle: 0
+Third cycle: 1      Third cycle: 1      Third cycle: 1
+Third cycle: 2      First cycle: 1      Second cycle: 1
+Second cycle: 1     Second cycle: 0     Third cycle: 0
+Third cycle: 0      Third cycle: 0      Third cycle: 1
+Third cycle: 1      Third cycle: 1      Second cycle: 2
+Third cycle: 2      First cycle: 2      Third cycle: 0
+Second cycle: 2     Second cycle: 0     Third cycle: 1
+Third cycle: 0      Third cycle: 0      First cycle: 1
+Third cycle: 1      Third cycle: 1      Second cycle: 0
+Third cycle: 2                          Third cycle: 0
+First cycle: 1                          Third cycle: 1
+Second cycle: 0                         Second cycle: 1
+Third cycle: 0                          Third cycle: 0
+Third cycle: 1                          Third cycle: 1
+Third cycle: 2                          Second cycle: 2
+Second cycle: 1                         Third cycle: 0
+Third cycle: 0                          Third cycle: 1
+Third cycle: 1                          First cycle: 2
+Third cycle: 2                          Second cycle: 0
+Second cycle: 2                         Third cycle: 0
+Third cycle: 0                          Third cycle: 1
+Third cycle: 1                          Second cycle: 1
+Third cycle: 2                          Third cycle: 0
+First cycle: 2                          Third cycle: 1
+Second cycle: 0                         Second cycle: 2
+Third cycle: 0                          Third cycle: 0
+Third cycle: 1                          Third cycle: 1
+Third cycle: 2
+Second cycle: 1
+Third cycle: 0
+Third cycle: 1
+Third cycle: 2
+Second cycle: 2
+Third cycle: 0
+Third cycle: 1
+Third cycle: 2
+*/
+
+
