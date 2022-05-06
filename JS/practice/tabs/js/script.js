2ca55097c1bac55af3732621d6797645f6e2bbfc
Vladyslav <ryzhykovvv@gmail.com>
2022-05-06 23:02:15 +0300

    created a timer for the course lesson

--- JS/practice/tabs/js/script.js
+++ JS/practice/tabs/js/script.js
@@ -1,4 +1,6 @@
 window.addEventListener('DOMContentLoaded', ()=> {
+  // Tabs
+
   const tabContent =document.querySelectorAll('.tabcontent');
   const tabItem =document.querySelectorAll('.tabheader__item');
   const tabsWrapper = document.querySelector('.tabheader__items');
@@ -39,10 +41,71 @@ window.addEventListener('DOMContentLoaded', ()=> {
       event.classList.add('tabheader__item_active');
     }
   });
+
+  // Timer
+
+  const deadline = '2022-05-16';
+
+  function getTimeRemaining(endtime) {
+    let days, hours, minutes, seconds;
+    const total = Date.parse(endtime) - Date.parse(new Date());
+
+    if (total <= 0) {
+      days = 0;
+      hours = 0;
+      minutes = 0;
+      seconds = 0;
+    } else {
+      days = Math.floor(total / (1000 * 60 * 60 * 24));
+      hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+      minutes = Math.floor((total / (1000 * 60)) % 60);
+      seconds = Math.floor((total / 1000) % 60);
+    }
+
+    return {
+      total,
+      days,
+      hours,
+      minutes,
+      seconds,
+    };
+  }
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector);
+    const days = timer.querySelector('#days');
+    const hours = timer.querySelector('#hours');
+    const minutes = timer.querySelector('#minutes');
+    const seconds = timer.querySelector('#seconds');
+    const timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+
+    function updateClock() {
+      const timeObj = getTimeRemaining(endtime);
+
+      days.innerHTML= getZero(timeObj.days);
+      hours.innerHTML= getZero(timeObj.hours);
+      minutes.innerHTML= getZero(timeObj.minutes);
+      seconds.innerHTML= getZero(timeObj.seconds);
+
+      if (timeObj.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  setClock('.timer', deadline);
+
+
 });
 
 
-/* if (event && event.classList.contains !== 'tabheader__item_active') */
-/* if (event.classList.contains !== 'tabheader__item_active') */
-
-/* if (event.closest() !== 'tabheader__item_active') ????*/
