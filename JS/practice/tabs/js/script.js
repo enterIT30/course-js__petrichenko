4d6cf6030db53d66248ef484861c7a780b665057
Vladyslav <ryzhykovvv@gmail.com>
2022-05-07 14:15:50 +0300

    fixing my version and adding a rest operator to the lecturer version

--- JS/practice/tabs/js/script.js
+++ JS/practice/tabs/js/script.js
@@ -183,15 +183,13 @@ window.addEventListener('DOMContentLoaded', ()=> {
       wrapperCards.append(newCard);
 
       newCard.innerHTML = `
-        <div class="${this.main}">
-          <img src="img/tabs/${this.path}.jpg" alt="${this.path}">
-          <h3 class="${this.main}-subtitle">Меню "${this.menu}"</h3>
-          <div class="${this.main}-descr">Меню "${this.menu}" - ${this.descr}</div>
-          <div class="${this.main}-divider"></div>
-          <div class="${this.main}-price">
-            <div class="${this.main}-cost">Цена:</div>
-            <div class="${this.main}-total"><span>${this.price}</span> грн/день</div>
-          </div>
+        <img src="img/tabs/${this.path}.jpg" alt="${this.path}">
+        <h3 class="${this.main}-subtitle">Меню "${this.menu}"</h3>
+        <div class="${this.main}-descr">Меню "${this.menu}" - ${this.descr}</div>
+        <div class="${this.main}-divider"></div>
+        <div class="${this.main}-price">
+          <div class="${this.main}-cost">Цена:</div>
+          <div class="${this.main}-total"><span>${this.price}</span> грн/день</div>
         </div>
       `;
     }
@@ -207,11 +205,12 @@ window.addEventListener('DOMContentLoaded', ()=> {
   // Lecturer's version
 
   class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector) {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
       this.src = src;
       this.alt = alt;
       this.title = title;
       this.descr = descr;
+      this.classes = classes;
       this.price = price;
       this.parent = document.querySelector(parentSelector);
       this.transfer = 27;
@@ -224,9 +223,15 @@ window.addEventListener('DOMContentLoaded', ()=> {
 
     render() {
       const element = document.createElement('div');
+      if (this.classes.length === 0) {
+        element.classList.add('menu__item');
+      } else {
+        this.classes.forEach(className => {
+          element.classList.add(className);
+        });
+      }
 
       element.innerHTML = `
-      <div class="menu__item">
         <img src=${this.src} alt=${this.alt}>
         <h3 class="menu__item-subtitle">${this.title}</h3>
         <div class="menu__item-descr">${this.descr}</div>
@@ -235,7 +240,6 @@ window.addEventListener('DOMContentLoaded', ()=> {
             <div class="menu__item-cost">Цена:</div>
             <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
         </div>
-      </div>
       `;
       this.parent.append(element);
     }
@@ -257,7 +261,22 @@ window.addEventListener('DOMContentLoaded', ()=> {
     "Меню “Вариант лектора”",
     'Меню “Вариант лектора” - эта карточка сделанна при помощи классов и это вариант лектора курса. Я же сделал предыдущую карточку, студеннческую. Есть разница в исполнении, но считаю, что суть классов я понял.',
     9,
-    '.menu .container'
+    '.menu .container',
+/*     'menu__item',
+    'test-class' */
+  ).render();
+
+  new MenuCard(
+    "img/tabs/vegy.jpg",
+    "vegy",
+    "Меню “Вариант rest”",
+    'Меню “Вариант rest” - эта карточка сделанна для того, чтобы проверить работу rest оператора. Здесь заданы два класса и эти классы отображаются. В пердыдущей карточке, классы не задавались в параметрах. Проверка работы по умолчанию.',
+    9,
+    '.menu .container',
+    'menu__item',
+    'test-class'
   ).render();
 
 });
+
+
