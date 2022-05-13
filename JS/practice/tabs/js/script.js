ff703b0a3bc3c28e07879df1f2162fc91c0daf23
Vladyslav <ryzhykovvv@gmail.com>
2022-05-13 14:22:40 +0300

    added a spinner to the form for the course lesson

--- JS/practice/tabs/js/script.js
+++ JS/practice/tabs/js/script.js
@@ -109,7 +109,6 @@ window.addEventListener('DOMContentLoaded', ()=> {
 
   const buttons = document.querySelectorAll('[data-modal]');
   const modal = document.querySelector('.modal');
-  const buttonClose = document.querySelector('.modal__close');
 
   buttons.forEach(btn => {
     btn.addEventListener('click', () => {
@@ -133,12 +132,9 @@ window.addEventListener('DOMContentLoaded', ()=> {
     document.body.style.overflow = '';
   }
 
-  buttonClose.addEventListener('click', closeModal);
-
-
   modal.addEventListener('click', (e) => {
 
-    if (e.target === modal) {
+    if (e.target === modal || e.target.getAttribute('data-close') == '') {
       closeModal();
     }
   });
@@ -149,7 +145,7 @@ window.addEventListener('DOMContentLoaded', ()=> {
     }
   });
 
-  const modalTimerId = setTimeout(openModal, 10000);
+  const modalTimerId = setTimeout(openModal, 50000);
 
   function showModalByScroll() {
     if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
@@ -277,11 +273,12 @@ window.addEventListener('DOMContentLoaded', ()=> {
     'test-class'
   ).render();
 
+
   // Form (почти ничего не понял)
 
   const forms = document.querySelectorAll('form');
   const message = {
-    loading: 'Загрузка...',
+    loading: 'img/form/spinner.svg',
     success: 'Спасибо! Скоро мы с вами свяжемся',
     failure: 'Что-то пошло не так...',
   };
@@ -294,10 +291,13 @@ window.addEventListener('DOMContentLoaded', ()=> {
     form.addEventListener('submit', (e) => {
       e.preventDefault();
 
-      const statusMessage = document.createElement('div');
-      statusMessage.classList.add('status');
-      statusMessage.textContent = message.loading;
-      form.appendChild(statusMessage);
+      const statusMessage = document.createElement('img');
+      statusMessage.src = message.loading;
+      statusMessage.style.cssText = `
+        display: block;
+        margin: 0 auto;
+      `;
+      form.insertAdjacentElement('afterend', statusMessage);
 
       const request = new XMLHttpRequest();
       request.open('POST', 'server.php');
@@ -317,16 +317,40 @@ window.addEventListener('DOMContentLoaded', ()=> {
       request.addEventListener('load', () => {
         if (request.status === 200) {
           console.log(request.response);
-          statusMessage.textContent = message.success;
+          showThanksModal(message.success);
           form.reset();
-          setTimeout(() => {
-            statusMessage.remove();
-          }, 2000);
+          statusMessage.remove();
         } else {
-          statusMessage.textContent = message.failure;
+          showThanksModal(message.failure);
         }
       });
     });
+  }
+
+  //Spinner form and ThanksModal
+
+  function showThanksModal(massage) {
+    const prevModalDialog = document.querySelector('.modal__dialog');
+
+    prevModalDialog.classList.add('hide');
+    openModal();
+
+    const thanksModal = document.createElement('div');
+    thanksModal.classList.add('modal__dialog');
+    thanksModal.innerHTML = `
+      <div class="modal__content">
+        <div class="modal__close" data-close>&times;</div>
+        <div class="modal__title">${massage}</div>
+      </div>
+    `;
+
+    modal.append(thanksModal);
+    setTimeout(() => {
+      thanksModal.remove();
+      prevModalDialog.classList.add('show');
+      prevModalDialog.classList.remove('hide');
+      closeModal();
+    }, 4000);
   }
 });
 
